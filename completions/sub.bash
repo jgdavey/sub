dd9a80c9b8f9167529de6a03c39a504767dea2ad
Joshua Davey <josh@joshuadavey.com>
2025-03-20 17:19:05 -0400

    Add some helper env vars for completion

--- completions/sub.bash
+++ completions/sub.bash
@@ -6,7 +6,7 @@ _sub() {
   if [ "$COMP_CWORD" -eq 1 ]; then
     completions="$(sub completions --commands | sed -e 's/\[.*//g')"
   else
-    export LASTCOMPWORD="$word"
+    export COMP_WORD="$word"
     completions="$(sub completions "${COMP_WORDS[1]}" "${COMP_WORDS[@]:2}" | sed -e 's/\[.*//g')"
   fi
   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
